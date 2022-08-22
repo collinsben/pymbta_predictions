c9ddc12c0e4840c8bd2b46dd0a754fabcded2cf3
Ben Collins <bcollins7982@gmail.com>
2022-08-21 22:57:31 -0400

    Restructure

--- src/python/mbta_interface/mbta.py
+++
@@ -1,8 +0,0 @@
-# Mappings between common names and MBTA keys
-
-place_keys = {
-  'central': 'place-cntsq',
-  'park street': 'place-pktrm',
-}
-
-# https://api-v3.mbta.com/alerts?sort=lifecycle&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE&filter%5Broute_type%5D=1&filter%5Broute%5D=Red&filter%5Bstop%5D=place_cntsq
